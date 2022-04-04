1d6ca747c46b33fdc97cb9311b0b24f1aff57ecf
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-04-04 21:30:55 +0700

    caching HTTP response data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,7 +27,11 @@ export default {
     context.commit('registerCoach', { ...coachData, id: userId });
   },
 
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       `https://vue-http-demo-29555-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
     );
@@ -53,5 +57,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
